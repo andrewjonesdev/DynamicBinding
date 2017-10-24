337288a1d7e18738b9f40ebeb2bd9e9edea23e9e
Andrew Jones <jones.gaffney1920@gmail.com>
2017-10-24 00:13:27 -0500

    Add project files.

---
+++ DynamicBinding/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace DynamicBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Mosh";
+            //obj.GetHashCode();
+            //alternative to dynamic is reflection, dynamic is the simplified alternative used in place of reflection
+            var methodinfo = obj.GetType().GetMethod("GetHashCode");
+            methodinfo.Invoke(null, null);
+
+            //handles it all in run time
+            dynamic excelObject = "mosh";
+            excelObject.Optimize();
+
+            dynamic name = "Mosh";
+            name = 10;
+
+            //When using dynamics more testing should be done since errors occur at runtime rather than compile time
+
+            dynamic a = 10;
+            dynamic b = 5;
+            //when using dynamic variables in an expression the expressions variable will be dynamic as well
+            var c = a + b;
+
+            //if the runtime type is implicitly convertible to the type storing it then it does not need to be cast
+            int i = 5;
+            dynamic d = i;
+            long l = d;
+        }
+    }
+}
